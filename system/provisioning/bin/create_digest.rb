ee7e7935548ff81249a8e02ce10aa2f38f439830
Chetan Sarva <chetan@pixelcop.net>
2012-02-20 19:24:34 -0500

    added digest for each bundle

---
+++ system/provisioning/bin/create_digest.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'digest'
+require 'json'
+
+path = ARGV.join(" ")
+if not File.exist? path then
+  puts "path '#{path}' does not exist"
+  exit 1
+end
+
+if not File.exist? "#{path}/manifest.json" then
+  puts "path '#{path}/manifest.json' does not exist"
+  exit 1
+end
+
+path = File.expand_path(path)
+sha = Digest::SHA2.new
+bundle_sha = Digest::SHA2.new
+
+digests = []
+Dir.glob("#{path}/**/*").sort.each do |f|
+  next if File.directory? f || File.basename(f) == "digest"
+  bundle_sha.file(f)
+  sha.reset()
+  digests << { :file => f.gsub(/#{path}\//, ''), :digest => sha.file(f).hexdigest() }
+end
+
+File.new(path+"/digest", 'w').write(JSON.pretty_generate({ :digest => bundle_sha.hexdigest(), :files => digests }) + "\n")
