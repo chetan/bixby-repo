8024a994359fa089bbb5e6246624ee10917508d3
Chetan Sarva <chetan@pixelcop.net>
2013-02-01 23:52:58 -0500

    ignore hidden dirs

--- update_digests.sh
+++ update_digests.sh
@@ -1,1 +1,4 @@
-find $(dirname $(readlink -f $0)) -mindepth 2 -maxdepth 2 -type d  | grep -v ./.git | xargs -n 1 ./system/provisioning/bin/create_digest.rb
+
+cd $(dirname $(readlink -f $0))
+find . -mindepth 2 -maxdepth 2 -type d  | egrep -v ^\\./\\. | xargs -n 1 ./system/provisioning/bin/create_digest.rb
+
