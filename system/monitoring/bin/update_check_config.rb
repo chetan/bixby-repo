97f924cdb34d03dbd155fffa0534a2df4e800b5e
Chetan Sarva <chetan@pixelcop.net>
2013-02-07 12:30:59 -0500

    rewrote update_check_config as a simple script

--- system/monitoring/bin/update_check_config.rb
+++ system/monitoring/bin/update_check_config.rb
@@ -1,28 +1,23 @@
 #!/usr/bin/env ruby
 
-require 'bixby-client'
+require "bixby-client/script"
 
-module Bixby
-module Monitoring
+path = Bixby.path("etc", "monitoring")
+FileUtils.mkdir_p(path)
 
-  class UpdateCheckConfig < Bixby::Command
+# copy stdin to config file
+config_file = File.join(path, "config.json")
+File.open(config_file, 'w') { |f| f.write(read_stdin()) }
 
-    def run
-      path = File.join(ENV["BIXBY_HOME"], "etc", "monitoring")
-      systemu("mkdir -p #{path}")
-      config_file = File.join(path, "config.json")
+# restart mon_daemon.rb
+#
+# for now we'll always restart to avoid code replacement
+# on check version updates. may reload in future..
+rpath = File.dirname(File.expand_path(__FILE__))
+status, stdout, stderr = systemu("#{rpath}/mon_daemon.rb restart")
 
-      File.open(config_file, 'w') { |f| f.write(read_stdin()) }
-
-      # restart mon_daemon.rb
-      #
-      # for now we'll always restart to avoid code replacement
-      # on check version updates. may reload in future..
-      rpath = File.dirname(File.expand_path(__FILE__))
-      systemu("#{rpath}/mon_daemon.rb --  restart")
-    end
-
-  end
-
-end # Monitoring
-end # Bixby
+debug {
+  CommandResponse.new({ :status => status.exitstatus,
+                        :stdout => stdout,
+                        :stderr => stderr }).to_s
+}
