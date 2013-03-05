d2157530087ef5e0149df9cf44d67e49e76e2b34
Chetan Sarva <chetan@pixelcop.net>
2013-03-05 18:43:32 -0500

    run when called directly; fix deps

--- hardware/storage/bin/monitoring/disk_usage.rb
+++ hardware/storage/bin/monitoring/disk_usage.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+use_bundle "hardware/storage"
+use_bundle "system/monitoring"
+
 require 'storage'
 require 'disk_usage'
 
@@ -43,3 +46,5 @@ module Monitoring
   end # Storage
 end # Monitoring
 end # Bixby
+
+Bixby::Monitoring::Storage::DiskUsage.new.run if $0 == __FILE__
