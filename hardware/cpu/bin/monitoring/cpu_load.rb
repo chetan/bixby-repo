ba65c8205e02aff205721f93905090c639eddded
Chetan Sarva <chetan@pixelcop.net>
2013-03-05 18:45:48 -0500

    run when called directly; fix deps

--- hardware/cpu/bin/monitoring/cpu_load.rb
+++ hardware/cpu/bin/monitoring/cpu_load.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+use_bundle "hardware/cpu"
+use_bundle "system/monitoring"
+
 module Bixby
 module Monitoring
   module CPU
@@ -23,3 +26,5 @@ module Monitoring
   end
 end # Monitoring
 end # Bixby
+
+Bixby::Monitoring::CPU::LoadAverage.new.run if $0 == __FILE__
