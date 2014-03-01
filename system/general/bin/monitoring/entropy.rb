97fd126e001c56c1d1999d56ff158455404b2ba6
Chetan Sarva <chetan@pixelcop.net>
2014-02-28 21:03:29 -0500

    [new] entropy check for linux

---
+++ system/general/bin/monitoring/entropy.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+use_bundle "system/monitoring"
+
+class EntropyAvail < Bixby::Monitoring::Base
+
+  def monitor
+    if linux? then
+      add_metric(:size => File.read("/proc/sys/kernel/random/entropy_avail").strip.to_i)
+    elsif darwin? then
+      # TODO darwin is not supported
+    end
+  end
+
+end
+
+Bixby::Monitoring::Base.subclasses.last.new.run if $0 == __FILE__
