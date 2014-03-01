b5ff2f8424b334bfba4da4e4bb9ca6c20fe4cf81
Chetan Sarva <chetan@pixelcop.net>
2014-02-28 21:08:05 -0500

    fixed metric name

--- system/general/bin/monitoring/entropy.rb
+++ system/general/bin/monitoring/entropy.rb
@@ -6,7 +6,7 @@ class EntropyAvail < Bixby::Monitoring::Base
 
   def monitor
     if linux? then
-      add_metric(:size => File.read("/proc/sys/kernel/random/entropy_avail").strip.to_i)
+      add_metric(:count => File.read("/proc/sys/kernel/random/entropy_avail").strip.to_i)
     elsif darwin? then
       # TODO darwin is not supported
     end
