e62ab2cdb230d8f70ae5efd03bf580c36ef987a4
Chetan Sarva <chetan@pixelcop.net>
2014-04-11 15:28:15 -0400

    exclude ec2_userdata from facts

--- system/inventory/bin/list_facts.rb
+++ system/inventory/bin/list_facts.rb
@@ -4,7 +4,9 @@ require 'rubygems'
 require 'facter'
 require 'multi_json'
 
-exclude_keys = %w(swapfree memoryfree)
+# ec2 userdata returns an array with one word per line for some reason, so just ignore it
+
+exclude_keys = %w(swapfree memoryfree ec2_userdata)
 facts = Facter.to_hash
 exclude_keys.each { |k| facts.delete(k) }
 
