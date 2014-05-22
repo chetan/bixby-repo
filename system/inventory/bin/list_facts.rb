a700daf76fbd990134a20ae2ec4fddebda05099f
Chetan Sarva <chetan@pixelcop.net>
2014-05-22 12:19:40 -0400

    ignore ssh pubkey and fingerprint facts

--- system/inventory/bin/list_facts.rb
+++ system/inventory/bin/list_facts.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# Run facter and return all facts as a JSON hash
+
 require 'rubygems'
 require 'facter'
 require 'multi_json'
@@ -7,7 +9,5 @@ require 'multi_json'
 # ec2 userdata returns an array with one word per line for some reason, so just ignore it
 
 exclude_keys = %w(swapfree memoryfree ec2_userdata)
-facts = Facter.to_hash
-exclude_keys.each { |k| facts.delete(k) }
-
+facts = Facter.to_hash.reject{ |k,v| exclude_keys.include?(k) || k =~ /ssh.*key|sshfp_.*/ }
 puts MultiJson.dump(facts)
