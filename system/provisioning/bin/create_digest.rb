f9209762fda61131751ef8d9320bfdd975f1d7a8
Chetan Sarva <chetan@pixelcop.net>
2014-06-23 17:01:01 -0400

    valiate json files before updating digests

--- system/provisioning/bin/create_digest.rb
+++ system/provisioning/bin/create_digest.rb
@@ -3,9 +3,11 @@
 require 'bundler/setup'
 require 'bixby-common'
 require 'digest'
-require 'json'
+require 'multi_json'
+require 'oj'
 
 path = ARGV.join(" ")
+path = "." if path.nil? or path.empty?
 if not File.exist? path then
   puts "path '#{path}' does not exist"
   exit 1
@@ -18,6 +20,25 @@ end
 
 path.gsub!(%r{^\./}, '')
 
+puts "updating bundle digest for #{path}"
+
 ENV["BIXBY_HOME"] = Dir.pwd
-spec = Bixby::CommandSpec.new(:bundle => path, :repo => "..")
+
+# validate all json files
+errors = false
+Dir.glob(File.join(path, "**/*.json"), File::FNM_CASEFOLD).each do |f|
+  begin
+    MultiJson.load(File.read(f))
+  rescue MultiJson::ParseError => ex
+    warn "ERROR: invalid json file #{f}: #{ex.message}"
+    errors = true
+  end
+end
+if errors then
+  warn "aborting digest update due to errors"
+  exit 1
+end
+
+# update digest
+spec = Bixby::CommandSpec.new(:bundle => path, :repo => "..") # .. hack to fix paths
 spec.update_digest
