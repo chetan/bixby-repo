ef742bd3c9b51a43f16b13368a0340bfcf2df9c5
Chetan Sarva <chetan@pixelcop.net>
2014-07-04 16:16:31 -0400

    faster update_digests script

--- system/provisioning/bin/create_digest.rb
+++ system/provisioning/bin/create_digest.rb
@@ -6,39 +6,47 @@ require 'digest'
 require 'multi_json'
 require 'oj'
 
-path = ARGV.join(" ")
-path = "." if path.nil? or path.empty?
-if not File.exist? path then
-  puts "path '#{path}' does not exist"
-  exit 1
-end
+def create_digest(path)
 
-if not File.exist? "#{path}/manifest.json" then
-  puts "path '#{path}/manifest.json' does not exist"
-  exit 1
-end
-
-path.gsub!(%r{^\./}, '')
-
-puts "updating bundle digest for #{path}"
-
-ENV["BIXBY_HOME"] = Dir.pwd
-
-# validate all json files
-errors = false
-Dir.glob(File.join(path, "**/*.json"), File::FNM_CASEFOLD).each do |f|
-  begin
-    MultiJson.load(File.read(f))
-  rescue MultiJson::ParseError => ex
-    puts "ERROR: invalid json file #{f}: #{ex.message}"
-    errors = true
+  if not File.exist? path then
+    puts "path '#{path}' does not exist"
+    exit 1
   end
-end
-if errors then
-  puts "aborting digest update due to errors"
-  exit 1
+
+  if not File.exist? "#{path}/manifest.json" then
+    puts "path '#{path}/manifest.json' does not exist"
+    exit 1
+  end
+
+  path = path.gsub(%r{^\./}, '')
+
+  puts "updating bundle digest for #{path}"
+
+  ENV["BIXBY_HOME"] = Dir.pwd
+
+  # validate all json files
+  errors = false
+  Dir.glob(File.join(path, "**/*.json"), File::FNM_CASEFOLD).each do |f|
+    begin
+      MultiJson.load(File.read(f))
+    rescue MultiJson::ParseError => ex
+      puts "ERROR: invalid json file #{f}: #{ex.message}"
+      errors = true
+    end
+  end
+  if errors then
+    puts "aborting digest update due to errors"
+    exit 1
+  end
+
+  # update digest
+  spec = Bixby::CommandSpec.new(:bundle => path, :repo => "..") # .. hack to fix paths
+  spec.update_digest
+
 end
 
-# update digest
-spec = Bixby::CommandSpec.new(:bundle => path, :repo => "..") # .. hack to fix paths
-spec.update_digest
+paths = ARGV.to_a
+paths << "." if paths.empty?
+paths.each do |path|
+  create_digest(path)
+end
--- update_digests.sh
+++ update_digests.sh
@@ -6,5 +6,7 @@
 cd $(dirname $(readlink -f $0))
 for f in `find . -type f -name 'manifest.json' | egrep -v '^\./\.'`; do
   d=$(dirname $f);
-  ./system/provisioning/bin/create_digest.rb $d
+  dirs="$dirs $d"
 done
+
+./system/provisioning/bin/create_digest.rb $dirs
